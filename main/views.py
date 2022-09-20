387885aab64b7415d203a556858778c7e47519ab
dacaroglu <dacaroglu@gmail.com>
2022-09-19 23:03:13 -0400

    product page template created

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home (request):
+    return render(request, 'home.html')
+def products(request):
+    return render(request,'shop/index.html')
